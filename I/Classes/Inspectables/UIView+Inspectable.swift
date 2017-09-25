2ba84c58d8c49c1b0d8a8301a23107bba4d527c2
miup <contact@miup.blue>
2017-09-25 15:20:01 +0900

    add shadow

--- I/Classes/Inspectables/UIView+Inspectable.swift
+++ I/Classes/Inspectables/UIView+Inspectable.swift
@@ -8,13 +8,82 @@
 import UIKit
 
 extension UIView {
+    @IBInspectable
+    var iMasksToBounds: Bool {
+        get {
+            return layer.masksToBounds
+        }
+        set {
+            layer.masksToBounds = newValue
+        }
+    }
+    @IBInspectable
     var iCornerRadius: CGFloat {
         get {
             return layer.cornerRadius
         }
         set {
-            layer.masksToBounds = newValue >= 0
             layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var iBorderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable
+    var iBorderColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.borderColor ?? UIColor.clear.cgColor)
+        }
+        set {
+            layer.borderColor = newValue.cgColor
+        }
+    }
+
+    @IBInspectable
+    var iShadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var iShadowColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.shadowColor ?? UIColor.clear.cgColor)
+        }
+        set {
+            layer.shadowColor = newValue.cgColor
+        }
+    }
+
+    @IBInspectable
+    var iShadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable
+    var iShadowOpacity: CGFloat {
+        get {
+            return CGFloat(layer.shadowOpacity)
+        }
+        set {
+            layer.shadowOpacity = Float(newValue)
         }
     }
 }
